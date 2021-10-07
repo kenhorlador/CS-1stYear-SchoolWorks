f977c607861d9ae9ba47bc0b46909cdf8741c613
Ken Barrameda Horlador <kenhorlador@gmail.com>
2021-10-07 22:50:17 +0800

    decrease the storage for empcode to just three-digit and add documentations

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+// Include stdio header to support input and output functions
 #include <stdio.h>
 
 /*
+ <!-- INSTRUCTION-->
+
   Write a program that will compute the monthly salary of an employee.
   The user needs to enter the employee code (three-digit-code), the hourly
   rate of the employee,and the number of hours worked. Then display the
@@ -18,6 +21,7 @@
   Rate per Hour: 200
   Number of hours worked: 40
   Salary of Employee: Php 8000.00
+
 */
 
 int main() {
@@ -27,28 +31,32 @@ int main() {
     float ratePerHour, hrsWorked, salaryOfEmployee;
 
     // Grab the Employee Code
-    printf("\nEmployee Code:");
+    printf("\nEnter your Employee Code(only accepts three-digit code):");
     scanf("%s", empCode);
 
-    // Rate Per Hour
+    // Grab the Employee's Rate Per Hour
     printf("Rate per Hour:");
     scanf("%f", &ratePerHour);
 
-    // Number of hours worked
+    // Grab the Number of hours worked
     printf("Number of hours worked:");
     scanf("%f", &hrsWorked);
 
+    /*
+     Calculate the employee's salary based on his/her
+     rate per hour and total hours worked
+     * */
     salaryOfEmployee = ratePerHour * hrsWorked;
 
     // Space to separate the input value and the output
     printf("\n\n");
 
-    printf("******************** B I L L ********************\n"
+    printf("******************** B I L L **********************\n"
            "Employee Code      Rate per Hour         Hrs Worked\n"
            "%s                %.2f                 %.2f\n"
-           "-------------------------------------------------\n"
+           "---------------------------------------------------\n"
            "Salary of Employee: %.2f\n"
-           "-------------------------------------------------\n",
+           "---------------------------------------------------\n",
            empCode, ratePerHour, hrsWorked, salaryOfEmployee);
 
     return 0;
